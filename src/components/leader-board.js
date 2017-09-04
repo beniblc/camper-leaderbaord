c1706ca4099847c85799e95620d37bbcef6e6fb6
Ben Crisostomo <beni.blc@gmail.com>
2017-09-04 00:21:19 -0400

    added files

--- src/components/leader-board.js
+++ src/components/leader-board.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+class Leaderboard extends Component {
   render() {
     return (
       <div>React simple starter</div>
     );
   }
 }
+
+export default Leaderboard
